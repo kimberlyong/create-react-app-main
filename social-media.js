9b4211ba5ca79e71761b103786a78246b07f39d5
nikitawarrier1 <nikitawarrier@gmail.com>
2024-09-07 13:30:36 -0700

    social media page

---
+++ social-media.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './social-media.css'; 
+
+function SocialMedia() {
+  return (
+    <div className="social-media-container">
+      <h1>Here's what you missed</h1>
+    </div>
+  );
+}
+
+export default SocialMedia;
