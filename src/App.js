c52d1022cd296e2639e22ba1e1499a75eed28d28
kimberlyong <kimberlyong@outlook.com>
2024-09-07 12:00:20 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>bye</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
